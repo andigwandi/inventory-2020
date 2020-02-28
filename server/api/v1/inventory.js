656ba28725fa1f02887c3d4d31344ba625b9c3bd
Sanjeev Kumar <skumar274@sapient.com>
2020-02-28 12:07:27 +0530

    inventory api basic

---
+++ server/api/v1/inventory.js
@@ -0,0 +1,91 @@
+var app = require("express")();
+var server = require("http").Server(app);
+var bodyParser = require("body-parser");
+var Datastore = require("nedb");
+var async = require("async");
+
+app.use(bodyParser.json());
+
+module.exports = app;
+
+// Creates  Database
+var inventoryDB = new Datastore({
+  filename: "./server/databases/inventory.db",
+  autoload: true
+});
+
+// GET inventory
+app.get("/", function(req, res) {
+  res.send("Inventory API");
+});
+
+// GET a product from inventory by _id
+app.get("/product/:productId", function(req, res) {
+    if (!req.params.productId) {
+      res.status(500).send("ID field is required.");
+    } else {
+      inventoryDB.findOne({ _id: req.params.productId }, function(err, product) {
+        res.send(product);
+      });
+    }
+  });
+  
+  // GET all inventory products
+  app.get("/products", function(req, res) {
+    inventoryDB.find({}, function(err, docs) {
+      console.log("sending inventory products");
+      res.send(docs);
+    });
+  });
+  
+//   // GET all inventory products
+//   app.get("/products/expiry", function(req, res) {
+//     inventoryDB.find({ expdate: { $lt: { $gte: moment().startOf("month").toDate().toJSON() } }}, function(err, docs) {
+//       console.log("sending inventory products");
+//       res.send(docs);
+//     });
+//   });
+  
+
+// post inventory product
+app.post("/product", function(req, res) {
+    var newProduct = req.body;
+  
+    inventoryDB.insert(newProduct, function(err, product) {
+      if (err) res.status(500).send(err);
+      else res.send(product);
+    });
+  });
+  
+  //delete product using product id
+  app.delete("/product/:productId", function(req, res) {
+    inventoryDB.remove({ _id: req.params.productId }, function(err, numRemoved) {
+      if (err) res.status(500).send(err);
+      else res.sendStatus(200);
+    });
+  });
+  
+  // Updates inventory product
+  app.put("/product", function(req, res) {
+    var productId = req.body._id;
+  
+    inventoryDB.update({ _id: productId }, req.body, {}, function(
+      err,
+      numReplaced,
+      product
+    ) {
+      if (err) res.status(500).send(err);
+      else res.sendStatus(200);
+    });
+  });
+
+// post inventory product
+app.post("/product", function(req, res) {
+    var newProduct = req.body;
+  
+    inventoryDB.insert(newProduct, function(err, product) {
+      if (err) res.status(500).send(err);
+      else res.send(product);
+    });
+  });
+  
---
+++ server/index.js
@@ -0,0 +1,36 @@
+var express = require("express"),
+  http = require("http"),
+  app = require("express")(),
+  server = http.createServer(app),
+  bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 8001;
+
+console.log("Server started");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.all("/*", function(req, res, next) {
+//   // CORS headers
+//   res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//   // Set custom headers for CORS
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-type,Accept,X-Access-Token,X-Key"
+//   );
+//   if (req.method == "OPTIONS") {
+//     res.status(200).end();
+//   } else {
+//     next();
+//   }
+// });
+
+app.get("/", function(req, res) {
+  res.send(" Inventory web app running.");
+});
+
+app.use("/api/v1/inventory", require("./api/v1/inventory"));
+
+
+server.listen(PORT, () => console.log(` Listening on PORT ${PORT}`));
